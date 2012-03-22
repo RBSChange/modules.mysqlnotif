6920c90088d99a192e56a8347c4c3e85cb5c6609
Gaël PORT <gael.port@rbs.fr>
2012-03-22 16:57:40 +0100

    [REFACTOR] Use addInjectionInProjectConfiguration.

--- setup/initData.php
+++ setup/initData.php
@@ -6,16 +6,8 @@ class mysqlnotif_Setup extends object_InitDataSetup
 {
 	public function install()
 	{
-		$nnsName = Framework::getConfigurationValue('injection/notification_NotificationService', 'notification_NotificationService');
-		if ($nnsName != 'notification_NotificationService' && $nnsName != 'mysqlnotif_InjectedNotificationService')
-		{
-			$this->addWarning($nnsName . ' must be extend mysqlnotif_InjectedNotificationService !');
-		}
-		else
-		{
-			$this->addProjectConfigurationEntry('injection/notification_NotificationService', 'mysqlnotif_InjectedNotificationService');
-		}		
-		mysqlnotif_ModuleService::getInstance()->addBackgroundSendNotifTask();
+		// Add injection of notification_NotificationService.
+		$this->addInjectionInProjectConfiguration('notification_NotificationService', 'mysqlnotif_InjectedNotificationService');
 	}
 
 	/**
